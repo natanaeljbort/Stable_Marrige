30fc218925500f19e5e2e7c4703c8e5b22e86892
natanaeljbort <45204945+natanaeljbort@users.noreply.github.com>
2019-12-02 23:43:31 +0100

    Update stable marrige.py

--- stable marrige.py
+++ stable marrige.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Nov 27 15:37:24 2019
 
@@ -6,19 +5,14 @@ Created on Wed Nov 27 15:37:24 2019
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 import random as ra
 
 n=4
-#suffels=5
 Boys=np.zeros((n,n))
 Girls=np.zeros((n,n))
 Match=np.zeros((n))
 Rang=np.zeros((n))
 Repeat=np.zeros((n))
-Match1=np.zeros((n))
-Rang1=np.zeros((n))
 
 def crear_matriu_inicial(n):
     Matrix=np.zeros((n,n))
@@ -27,7 +21,6 @@ def crear_matriu_inicial(n):
             Matrix[i][j]=j
 
     for i in range(0,n):
-        #Rand1=suffels*n
         for j in range(0,n):
             Rand2=int((n-1)*ra.random())
             g1=int(Matrix[i][j])
@@ -36,18 +29,18 @@ def crear_matriu_inicial(n):
             Matrix[i][j]=g2
     for i in range(0,n):
         for j in range(0,n):
-            Matrix[i][j]=Matrix[i][j]+1
+            Matrix[i][j]=Matrix[i][j]
     return Matrix
 
 def Match_Rang_creation(Boys, Girls, n):
 
     for i in range(0,n):
-        k = int(Boys[i][1])
+        k = int(Boys[i][0])
         for q in range(0,n):
-            w = int(Girls[k-1][q])-1
+            w = int(Girls[k][q])
             if(w == i):
                 Match[i]=k
-                Rang[i]=q+1
+                Rang[i]=q
 
 def totes_diferents(V, n):
     k=0
@@ -62,12 +55,8 @@ def totes_diferents(V, n):
             break
 def Rango(Girls, n, girl, boy):
     for j in range(0,n):
-        if(Girls[boy][j]==girl):
+        if(Girls[girl][j]==boy):
             return j
-
-def zero_repeat(n):
-    for i in range(0,n):
-        Repeat[i]=0
 
 def comparar(Boys, Girls, n):
     
@@ -76,32 +65,23 @@ def comparar(Boys, Girls, n):
     for j in range(0,n-1):
         h=totes_diferents(Match, n)
         if(h!=1):
-            break
-        zero_repeat(n)        
+            break      
         for i in range(0,n):
             for g in range(0,n):
-                if(g==i or Repeat[g]==1):
+                print(Match)
+                if(g==i):
                     continue
-                elif(Match[i]==Match[g]):
-                    if(Rang[i]>Rang[g]):
-                        Match[i]=Boys[i][j+1]
-                        Rang[i]=Rango(Girls, n, int(Match[i]), i)
-                        Repeat[i]=1
-                        break
-
+                elif(Match[i]==Match[g] and Rang[i]>Rang[g]):
+                    Match[i]=Boys[i][j+1]
+                    Rang[i]=Rango(Girls, n, int(Match[i]), i)
+                    break
     return Match
+
 def main():
     Boys=crear_matriu_inicial(n)
     Girls=crear_matriu_inicial(n)
-    print(Boys)
-    print(Girls)
-    
-    
     Match=comparar(Boys, Girls, n)
     print(Rang)
     print(Match)
     
-    
 main()
-
-
