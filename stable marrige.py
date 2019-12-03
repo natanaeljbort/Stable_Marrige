778905e47f075b4d40b9a255d708a7117ca353c8
natanaeljbort <45204945+natanaeljbort@users.noreply.github.com>
2019-12-03 11:39:34 +0100

    Update stable marrige.py

--- stable marrige.py
+++ stable marrige.py
@@ -7,13 +7,14 @@ Created on Wed Nov 27 15:37:24 2019
 import numpy as np
 import random as ra
 
-n=4
+n=10
 Boys=np.zeros((n,n))
 Girls=np.zeros((n,n))
 Match=np.zeros((n))
 Rang=np.zeros((n))
 Repeat=np.zeros((n))
 
+#Function that creates random matrices in order to have initial testing data .
 def crear_matriu_inicial(n):
     Matrix=np.zeros((n,n))
     for i in range(0,n):
@@ -32,6 +33,7 @@ def crear_matriu_inicial(n):
             Matrix[i][j]=Matrix[i][j]
     return Matrix
 
+#Function that creates the initial Match and Rang vectors, which will be modified as the algorithm progresses.
 def Match_Rang_creation(Boys, Girls, n):
 
     for i in range(0,n):
@@ -41,7 +43,7 @@ def Match_Rang_creation(Boys, Girls, n):
             if(w == i):
                 Match[i]=k
                 Rang[i]=q
-
+#Function that tells us when the solution is found (everybody has a different partner).
 def totes_diferents(V, n):
     k=0
     for i in range(0,n):
@@ -53,11 +55,13 @@ def totes_diferents(V, n):
                 break
         if(k==1):
             break
+#Function that tells us the rang that a boy has for a given girl.
 def Rango(Girls, n, girl, boy):
     for j in range(0,n):
         if(Girls[girl][j]==boy):
             return j
-
+#Function that finds the solution given the two initial matrices of boys and girls. This is done by modifying the Match ang Rang vector 
+#by comparing between the preferences of all the boys and girls.
 def comparar(Boys, Girls, n):
     
     Match_Rang_creation(Boys, Girls, n)
@@ -75,7 +79,7 @@ def comparar(Boys, Girls, n):
                     Rang[i]=Rango(Girls, n, int(Match[i]), i)
                     break
     return Match
-
+#Main function.
 def main():
     Boys=crear_matriu_inicial(n)
     Girls=crear_matriu_inicial(n)
